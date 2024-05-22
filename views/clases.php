bcfca46c918c626a903437b556fb2844ebe8f935
Evelynuwu3456 <evelyn2night3@gmail.com>
2024-05-21 23:55:16 -0300

    subo mini cambio de dise a las card de clases

--- views/clases.php
+++ views/clases.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="./css/clases.css">
 <div class="container mt-4">
     <h1>Mis Clases</h1>
     <br>
@@ -5,8 +6,9 @@
         <div class="col-md-4 mb-4">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Clase de Matemáticas</h5>
+                    <h3 class="card-title">Clase de Matemáticas</h3>
                     <h6 class="card-subtitle mb-2 text-muted">Profesor: Gabriel Valera</h6>
+                    <hr>
                     <p class="card-text">Esta clase cubrirá temas avanzados de álgebra y cálculo.</p>
                     <p class="card-text">Fecha y hora: Martes y Jueves, 10:00 AM - 12:00 PM</p>
                     <a href="#" class="btn btn-primary">Ir a la clase</a>
@@ -16,8 +18,9 @@
         <div class="col-md-4 mb-4">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Clase de Literatura</h5>
+                    <h3 class="card-title">Clase de Literatura</h3>
                     <h6 class="card-subtitle mb-2 text-muted">Profesor: David Martínez</h6>
+                    <hr>
                     <p class="card-text">Analisis de las obras literarias más destacadas de la historia.</p>
                     <p class="card-text">Fecha y hora: Martes y Jueves, 11:00 AM - 1:00 PM</p>
                     <a href="#" class="btn btn-primary">Ir a la clase</a>
@@ -27,8 +30,9 @@
         <div class="col-md-4 mb-4">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Clase de Química</h5>
+                    <h3 class="card-title">Clase de Química</h3>
                     <h6 class="card-subtitle mb-2 text-muted">Profesor: Ana García</h6>
+                    <hr>
                     <p class="card-text">Explora los principios fundamentales de la química y las reacciones químicas.</p>
                     <p class="card-text">Fecha y hora: Lunes y Miércoles, 9:00 AM - 11:00 AM</p>
                     <a href="#" class="btn btn-primary">Ir a la clase</a>
@@ -38,8 +42,9 @@
         <div class="col-md-4 mb-4">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Clase de Geografía</h5>
+                    <h3 class="card-title">Clase de Geografía</h3>
                     <h6 class="card-subtitle mb-2 text-muted">Profesor: Laura Sánchez</h6>
+                    <hr>
                     <p class="card-text">Explora los diferentes aspectos de la geografía mundial, desde la cartografía hasta el clima y la demografía.</p>
                     <p class="card-text">Fecha y hora: Miércoles y Viernes, 10:00 AM - 12:00 PM</p>
                     <a href="#" class="btn btn-primary">Ir a la clase</a>
