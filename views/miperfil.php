72a8bc2c2119e296a64584b24ae9383d0abebcf9
Evelynuwu3456 <evelyn2night3@gmail.com>
2024-05-30 16:41:10 -0300

    subo arreglos de miperfil

--- views/miperfil.php
+++ views/miperfil.php
@@ -7,14 +7,13 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css
 ">
 <link rel="stylesheet" href="../css/miperfil.css">
-
-https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css
+<br>
 <div class="container mt-4 mb-4 p-3 d-flex justify-content-center">
     <div class="card p-4">
         <div class=" image d-flex flex-column justify-content-center align-items-center"> <button
-                class="btn btn-secondary"> <img src="https://i.imgur.com/wvxPV9S.png" height="100"
-                    width="100" /></button> <span class="name mt-3">Eleanor Pena</span> 
-                    <span class="idd">@eleanorpena</span>
+                class="btn btn-secondary"> <img src="../img/eve.jpg" height="100"
+                    width="100" /></button> <span class="name mt-3">Torres Evelyn</span> 
+                    <span class="idd">eve@gmail.com</span>
             <div class="d-flex flex-row justify-content-center align-items-center gap-2"> <span
                     class="idd1">Oxc4c16a645_b21a</span> <span><i class="fa fa-copy"></i></span> </div>
             <div class="d-flex flex-row justify-content-center align-items-center mt-3"> <span class="number">5 <span
@@ -22,10 +21,11 @@ https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.c
             <div class=" d-flex mt-2"> <button class="btn1 btn-dark">Editar Perfil</button> </div>
             <div class="text mt-3"> <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital
                     artwork.<br><br> Artist/ Creative Director by Day #NFT minting@ with FND night. </span> </div>
-            <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center"> <span><i
+            <!---div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center"> <span><i
                         class="fa fa-twitter"></i></span> <span><i class="fa fa-facebook-f"></i></span> <span><i
-                        class="fa fa-instagram"></i></span> <span><i class="fa fa-linkedin"></i></span> </div>
-            <div class=" px-2 rounded mt-4 date "> <span class="join">Joined May,2021</span> </div>
+                        class="fa fa-instagram"></i></span> <span><i class="fa fa-linkedin"></i></span><i class="fa fa-whatsapp"></i> </div>
+            <!---div class=" px-2 rounded mt-4 date "> <span class="join">Joined May,2021</span> </div--->
+        <br>
         </div>
     </div>
 </div>
