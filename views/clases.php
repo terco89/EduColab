a69c52c2a3fde5749736b3f29a7aa139425b671e
LucaROshi <lucaoshiro@gmail.com>
2024-06-04 16:44:57 -0300

    arreglos

--- views/clases.php
+++ views/clases.php
@@ -1,10 +1,45 @@
 <link rel="stylesheet" href="./css/clases.css">
 <div class="container mt-4">
-    <h1>Mis Clases
-    <a href="clase_unirse.php" class="btn btn-secondary">Unirse a una clase</a>
-    <a href="clase2.php" class="btn btn-secondary">Crear una clase</a>
+    <form method="POST" action="crear_clase.php">
+        <h1>Mis Clases
+            <a href="clase_unirse.php" class="btn btn-secondary">Unirse a una clase</a>
+            <a href="#" class="btn btn-secondary" data-toggle="modal" data-target="#submitModal">Crear una clase</a>
+            <div class="modal fade" id="submitModal" tabindex="-1" role="dialog" aria-labelledby="submitModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="submitModalLabel">Crear una clase</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form>
+                                <div class="form-group">
+                                    <label for="taskTitle">Nombre de la clase</label>
+                                    <input type="text" class="form-control" id="taskTitle" name="nombre" placeholder="Nombre de la clase">
+                                </div>
+                                <div class="form-group">
+                                    <label for="taskTitle">Seccion</label>
+                                    <input type="text" class="form-control" id="taskTitle" name="seccion" placeholder="Seccion">
+                                </div>
+                                <div class="form-group">
+                                    <label for="taskTitle">Asignatura</label>
+                                    <input type="text" class="form-control" id="taskTitle" name="asignatura" placeholder="Asignatura">
+                                </div>
+                                <div class="form-group">
+                                    <label for="taskTitle">Descripcion</label>
+                                    <input type="text" class="form-control" id="taskTitle" name="descripcion" placeholder="Descripcion">
+                                </div>
+                                <input type="submit" value="subir">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </form>
     </h1>
-    
+
     <br>
     <div class="row">
         <div class="col-md-4 mb-4">
