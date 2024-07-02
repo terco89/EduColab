90bb590d52ec07271ce1993d3ce23838964f9317
sdmatayoshi <sdmatayoshi@gmail.com>
2024-07-02 14:05:31 -0300

    asd

--- clase2.php
+++ clase2.php
@@ -4,12 +4,14 @@ session_start();
 if(!isset($_SESSION["usuario"])){
     header("Location: index.php");
 }
+
 if(isset($_POST['bg'])){
     $bg=$_POST['bg'].".jpg";
     $sql="UPDATE clase_usuario SET fondo='".$bg."' WHERE id_usuario='".$_SESSION['usuario']['id']."'";
     $query=mysqli_query($link,$sql);
 }
 $sql="SELECT fondo FROM clase_usuario WHERE id_usuario='".$_SESSION['usuario']['id']."'";
+
 $query=mysqli_query($link,$sql);
 $fondo=mysqli_fetch_assoc($query);
 $view = "clase2";
