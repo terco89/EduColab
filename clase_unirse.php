f162a9d8f98fde7862081e06b00093b36ed2c9ce
terco89 <valeraquiroz.gb@gmail.com>
2024-07-11 14:54:28 -0300

    fix clase_unirse

--- clase_unirse.php
+++ clase_unirse.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION["usuario"])){
+if (!isset($_SESSION["usuario"])) {
     header("Location: index.php");
 }
 require_once "includes/config.php";
@@ -15,9 +15,31 @@ if (isset($_POST["codigoClase"])) {
         $row = mysqli_fetch_assoc($result);
         $id_clase = $row["id"];
 
-        $sql_insert = "INSERT INTO clase_usuario (id_usuario, id_clase) VALUES ('".$_SESSION["usuario"]["id"]."', '$id_clase')";
+        $sql_insert = "INSERT INTO clase_usuario (id_usuario, id_clase) VALUES ('" . $_SESSION["usuario"]["id"] . "', '$id_clase')";
 
         if (mysqli_query($link, $sql_insert)) {
+            $sql = "select id from tareas WHERE clase_id = " . $id_clase . " AND fecha_entrega > NOW()";
+            $query = mysqli_query($link, $sql);
+            $tareas = array();
+
+            if (mysqli_num_rows($query) > 0) {
+                while ($row = mysqli_fetch_assoc($query)) {
+                    $tareas[] = $row;
+                }
+            } else {
+                echo "aca";
+                die();
+                header("Location: clase.php?id=$id_clase");
+            }
+            $sql = "INSERT INTO tarea_usuario(tarea_id,usuario_id,estado) VALUES ";
+            for($i = 0; $i < count($tareas);$i++){
+                $sql .= "(".$tareas[$i]["id"].",".$_SESSION["usuario"]["id"].",1)";
+                if($i != count($tareas)-1){
+                    $sql .= " , ";
+                }
+            }
+            $query = mysqli_query($link, $sql);
+
             echo "Te has unido a la clase correctamente.";
             header("Location: clase.php?id=$id_clase");
         } else {
