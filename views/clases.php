a93462aca34bbbbd7be2a00fed0615b7004a2096
rojastamaraa <rojastamaranya@gmail.com>
2024-11-03 18:37:31 -0300

    Cambio css de views/clases.php

--- views/clases.php
+++ views/clases.php
@@ -1,15 +1,16 @@
 <link rel="stylesheet" href="./css/clases.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 <ul class="breadcrumb">
-  <li><a href="index.php">Home</a></li>
-  <li><a class="active" href="clases.php">Clases</a></li>
+    <li><a href="index.php">Home</a></li>
+    <li><a class="active" href="clases.php">Clases</a></li>
 </ul>
 <div class="container mt-4">
     <form method="POST" action="crear_clase.php">
-        <h1>Mis Clases</h1>
-        <a href="clase_unirse.php" class="btn btn-secondary">Unirse a una clase</a>
-        <a href="clase_archivada.php" class="btn btn-secondary">Clase Archivada</a>
-        <a href="#" class="btn btn-secondary" data-toggle="modal" data-target="#submitModal">Crear una clase</a>
+        <div class="clases-botones">
+            <a href="clase_unirse.php" class="btn btn-light">Unirse a una clase</a>
+            <a href="clase_archivada.php" class="btn btn-light">Clase Archivada</a>
+            <a href="#" class="btn btn-light" data-toggle="modal" data-target="#submitModal">Crear una clase</a>
+        </div>
         <div class="modal fade" id="submitModal" tabindex="-1" role="dialog" aria-labelledby="submitModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
@@ -85,21 +86,23 @@
         <div class="row">
             <?php if (count($clases) == 0) { ?>
                 <div class="no-clases">
-                    <img src="../img/books.png" alt="">
+                    <img src="./img/books.png" alt="">
+                    <h5>Aún no tienes clases</h5>
                 </div>
                 <?php } else {
                 foreach ($clases as $clase) { ?>
                     <div class="col-md-4 mb-4">
                         <a href="clase.php?id=<?php echo $clase['id']; ?>" class="card-link">
-                            <div class="card" style="height: 200px;">
-                         
+                            <div class="card card-clase">
+
                                 <div class="card-body card-banner" style="<?php if (preg_match('/\.(jpg|png)$/i', $clase['fondo'])): ?>background-image: url('img/fondos/<?php echo $clase['fondo']; ?>'); background-size: cover; background-position: center; background-repeat: no-repeat;<?php else: ?>background-color: <?php echo htmlspecialchars($clase['fondo']); ?>;<?php endif; ?>;">
-                                
+                                    <div class="overlay"></div>
                                     <h3 class="card-title"><?php echo $clase["nombre"] ?></h3>
-                                    <h6 class="card-subtitle mb-2 text-muted">
+                                    <h6 class="card-subtitle mb-2 ">
                                         Profesor: <?php echo ($clase['id_usuario_creador'] != $_SESSION['usuario']['id']) ? $clase["nombre_profesor"] . " " . $clase["apellido_profesor"] : "tú"; ?>
                                     </h6>
-                                    <hr>
+                                </div>
+                                <div class="card-horarios">
                                     <?php
                                     $horarios = $clase['horarios'];
                                     $numHorarios = count($horarios);
@@ -108,10 +111,10 @@
                                     for ($i = 0; $i < ($mostrarMas ? $maxHorarios : $numHorarios); $i++) {
                                         $hora_inicio = date('H:i', strtotime($horarios[$i]["hora_inicio"]));
                                         $hora_fin = date('H:i', strtotime($horarios[$i]["hora_fin"]));
-                                        echo "<li style=color:#6c757d;>" . $horarios[$i]["dia_semana"] . " de " . $hora_inicio . " a " . $hora_fin . "</li>";
+                                        echo "<p style=color:#6c757d;>" . $horarios[$i]["dia_semana"] . " de " . $hora_inicio . " a " . $hora_fin . "</p>";
                                     }
                                     if ($mostrarMas) {
-                                        echo "<li style=color:#6c757d;>...</li>";
+                                        echo "<p style=color:#6c757d;>...</p>";
                                     }
                                     ?>
                                 </div>
