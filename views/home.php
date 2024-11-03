f00f528a59a3e38979f8b36d05dc24c686cdb922
rojastamaraa <rojastamaranya@gmail.com>
2024-11-03 20:52:38 -0300

    cambios a views/index.hp

--- views/home.php
+++ views/home.php
@@ -7,25 +7,62 @@
         <a class="btn btn-primary btn-lg" href="login.php" style=" background-color: #56c6b7; border-color:#56c6b7;">Inicia sesion</a>
     </div>
     <div class="row">
-    <div class="col-md-6">
-        <div class="testimonial" style=" border-color:#56c6b7;">
-            <p>"EduCollab ha transformado la forma en que administro mis clases. ¡Es increíblemente fácil de usar y mis estudiantes adoran la plataforma!"</p>
-            <p><strong>Profesor X</strong></p>
+        
+        <div class="col-md-6">
+            <h2>Tutorial rápido</h2>
+            <p>Aprende cómo empezar a usar EduCollab con nuestro tutorial paso a paso. ¡Es fácil y rápido!</p>
+            <a class="btn btn-primary" style=" background-color: #56c6b7;border-color:#56c6b7;color:white;" href="#">Ver tutorial</a>
         </div>
+        <div class="col-md-6">
+            <div class="testimonial" style=" border-color:#8691EC;">
+                <p>"EduCollab ha transformado la forma en que administro mis clases. ¡Es increíblemente fácil de usar y mis estudiantes adoran la plataforma!"</p>
+                <p><strong>Profesor X</strong></p>
+            </div>
+        </div>
+
     </div>
-    <div class="col-md-6">
-        <h2>Tutorial rápido</h2>
-        <p>Aprende cómo empezar a usar EduCollab con nuestro tutorial paso a paso. ¡Es fácil y rápido!</p>
-        <a class="btn btn-primary" style=" background-color: #56c6b7;border-color:#56c6b7;color:white;"href="#">Ver tutorial</a>
-    </div>
-</div>
 </div>
 
 <style>
-        /* Estilos personalizados */
-        .testimonial {
-            border-left: 4px solid #007bff;
-            padding-left: 15px;
-            margin-bottom: 20px;
-        }
-    </style>
+    /* Estilos personalizados */
+    .testimonial {
+        border-left: 4px solid #007bff;
+        padding-left: 15px;
+        margin-bottom: 20px;
+    }
+
+    .jumbotron {
+        background-color: #CBD0F8;
+    }
+
+    .jumbotron h1 {
+        color: #8691EC;
+        font-weight: 700 !important;
+        text-transform: uppercase;
+        font-size: 2.5em !important;
+    }
+
+    .jumbotron p {
+        color: white;
+        font-weight: 500;
+        text-shadow:
+            -0.5px 0.5px 1px rgba(0, 0, 0, 0.2),
+            -1px 1px 2px rgba(50, 65, 139, 0.3);
+    }
+
+    .btn-primary {
+        background-image: linear-gradient(to right, #32418B, #A09AE5, #BA8CE9, #D598DA);
+        border: none;
+        transition: .5s;
+    }
+
+    .btn-primary:hover{
+        transform: scale(1.1);
+        background-image: linear-gradient(to right, #32418B, #A09AE5, #BA8CE9, #D598DA);
+    }
+
+    .jumbotron hr{
+        background-color: #8691EC;
+        height: 1px;
+    }
+</style>
